9032cf7f116e6ff389de887c5adc907c86949321
Muhammad Hashim Javed <53869097+MHashimJaved@users.noreply.github.com>
2023-02-28 15:12:29 +0500

    Create main4.dart
    This is the most powerful var data type where we learn that when we execute realtime data to check whether the data is stored or not in the mobile app

---
+++ main4.dart
@@ -0,0 +1,10 @@
+void main() {
+  var data;     //This is the most powerful var data type where we learn that when we execute realtime data to check whether the data is stored or not in the mobile app
+  data = {
+    'map' : 1, 'data': 2
+    };
+   
+  print(data['map']);
+  
+  
+}
